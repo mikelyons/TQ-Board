a98cbca2ffeaaaa1d5351c17631dcbcceb2e0c87
Mike Lyons <lyons.mr@gmail.com>
2011-02-26 01:12:41 -0800

    Added a simple users model, Controller next. from http://www.aidanf.net/rails_user_authentication_tutorial

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,17 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110211082052) do
+ActiveRecord::Schema.define(:version => 20110226063704) do
 
   create_table "posts", :force => true do |t|
     t.string   "name"
     t.string   "title"
     t.text     "content"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", :force => true do |t|
     t.datetime "created_at"
     t.datetime "updated_at"
   end
